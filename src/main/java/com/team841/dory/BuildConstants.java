a871033d4f2117a214160a3c9310cf1520dbc623
David Tian <dtian.github@outlook.com>
2025-03-15 22:15:02 -0700

    install AdvantageKit

---
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -0,0 +1,19 @@
+package com.team841.dory;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Dory";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "7819b7f2a2b021945e015ada3859c9197fd6ced4";
+  public static final String GIT_DATE = "2025-03-15 22:07:31 PDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-03-15 22:13:56 PDT";
+  public static final long BUILD_UNIX_TIME = 1742102036180L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
