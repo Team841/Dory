e34a9072ece523d07f655b6b83f86d1793d8da1c
Jakebiom3ch <jacobprado77@gmail.com>
2025-03-20 16:29:16 -0700

    setup jacob's computer,

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "f6e5040ce2a612d433d5ca21273419592591b5e2";
-  public static final String GIT_DATE = "2025-03-18 20:04:39 PDT";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "48ef1fefc2879c49d63c971c8d003bf94036e286";
+  public static final String GIT_DATE = "2025-03-19 23:06:49 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-19 21:06:31 PDT";
-  public static final long BUILD_UNIX_TIME = 1742443591706L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-20 16:28:00 PDT";
+  public static final long BUILD_UNIX_TIME = 1742513280891L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
