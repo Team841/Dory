4bc47e32beb35aa349ba309966c1be1548d3da88
David Tian <dtian.github@outlook.com>
2025-03-22 13:13:11 -0700

    udpdate Advantagekit

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 25;
-  public static final String GIT_SHA = "853d3ba35ab72ff16d2592b2634b28aa0e36f049";
-  public static final String GIT_DATE = "2025-03-22 11:49:51 PDT";
+  public static final int GIT_REVISION = 26;
+  public static final String GIT_SHA = "531f5ce3f8259c282b42b3ff820594ff6deb16fc";
+  public static final String GIT_DATE = "2025-03-22 13:09:37 PDT";
   public static final String GIT_BRANCH = "autoSnap";
-  public static final String BUILD_DATE = "2025-03-22 13:30:08 PDT";
-  public static final long BUILD_UNIX_TIME = 1742675408068L;
+  public static final String BUILD_DATE = "2025-03-22 13:12:28 PDT";
+  public static final long BUILD_UNIX_TIME = 1742674348698L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
