7d2e6bc62113581751a63633dd602dc164e415ce
David Tian <dtian.github@outlook.com>
2025-03-22 19:44:05 -0700

    Update at 'Sat Mar 22 19:44:05 PDT 2025'

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 41;
-  public static final String GIT_SHA = "c275e608d5a9e731270790c99444f525eb3d5a5b";
-  public static final String GIT_DATE = "2025-03-22 19:40:11 PDT";
+  public static final int GIT_REVISION = 42;
+  public static final String GIT_SHA = "d9dc26d2185d2a172f9cd31d7639aa5dcd053011";
+  public static final String GIT_DATE = "2025-03-22 19:40:30 PDT";
   public static final String GIT_BRANCH = "event_sfrAutoAim";
-  public static final String BUILD_DATE = "2025-03-22 19:40:14 PDT";
-  public static final long BUILD_UNIX_TIME = 1742697614759L;
+  public static final String BUILD_DATE = "2025-03-22 19:40:30 PDT";
+  public static final long BUILD_UNIX_TIME = 1742697630383L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
--- src/main/java/com/team841/dory/drive/Commands/AutoSnap.java
+++ src/main/java/com/team841/dory/drive/Commands/AutoSnap.java
@@ -91,12 +91,12 @@ public class AutoSnap extends Command {
 
     @Override
     public void end(boolean interrupted) {
-        this.drivetrain.setControl(this.drivetrain.m_robotSpeeds.withSpeeds(new ChassisSpeeds()));
+//        this.drivetrain.setControl(this.drivetrain.m_robotSpeeds.withSpeeds(new ChassisSpeeds()));
     }
 
     @Override
     public boolean isFinished() {
-        return this.vx.atSetpoint() && this.vy.atSetpoint();
+//        return this.vx.atSetpoint() && this.vy.atSetpoint();
     }
 
 
