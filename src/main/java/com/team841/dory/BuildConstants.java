ba08d2a5dd3844c781fbaa0eb460dc277bad18e3
P0 Programming <841student@gmail.com>
2025-03-22 18:43:48 -0700

    backing out and break mode for shooter and intake tested

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 29;
-  public static final String GIT_SHA = "07685f54c13be3655c8b591270005046d0d8eb90";
-  public static final String GIT_DATE = "2025-03-22 13:35:00 PDT";
+  public static final int GIT_REVISION = 30;
+  public static final String GIT_SHA = "7a6608ea904e23e306e3a0725a0b345402aafc84";
+  public static final String GIT_DATE = "2025-03-22 15:49:45 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-22 15:23:05 PDT";
-  public static final long BUILD_UNIX_TIME = 1742682185065L;
+  public static final String BUILD_DATE = "2025-03-22 18:41:48 PDT";
+  public static final long BUILD_UNIX_TIME = 1742694108156L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
