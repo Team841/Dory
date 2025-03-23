aea0698d4ed1a657a7573c6da36de00021ae5f24
David Tian <dtian.github@outlook.com>
2025-03-23 08:32:19 -0700

    Update at 'Sun Mar 23 08:32:19 PDT 2025'

--- src/main/java/com/team841/dory/drive/Commands/AutoSnap.java
+++ src/main/java/com/team841/dory/drive/Commands/AutoSnap.java
@@ -97,7 +97,7 @@ public class AutoSnap extends Command {
 
     @Override
     public boolean isFinished() {
-        return this.vx.atSetpoint() && this.vy.atSetpoint();
+        return this.vx.atGoal() && this.vy.atGoal();
 //        return false;
     }
 
