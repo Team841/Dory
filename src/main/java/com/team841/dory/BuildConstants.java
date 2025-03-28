6ec19df25d3500765eff27392bad4834e14f8e04
David Tian <dtian.github@outlook.com>
2025-03-28 09:37:25 -0700

    Update at 'Fri Mar 28 09:37:25 PDT 2025'

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 77;
-  public static final String GIT_SHA = "c4ed35c7dca90195c48b3058b179ca7d552cb50f";
-  public static final String GIT_DATE = "2025-03-28 09:34:43 PDT";
+  public static final int GIT_REVISION = 78;
+  public static final String GIT_SHA = "432516855078bca1878cc58f527ce3a64909576f";
+  public static final String GIT_DATE = "2025-03-28 09:35:17 PDT";
   public static final String GIT_BRANCH = "event_ccr";
-  public static final String BUILD_DATE = "2025-03-28 09:34:43 PDT";
-  public static final long BUILD_UNIX_TIME = 1743179683150L;
+  public static final String BUILD_DATE = "2025-03-28 09:35:17 PDT";
+  public static final long BUILD_UNIX_TIME = 1743179717336L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
--- src/main/java/com/team841/dory/RobotContainer.java
+++ src/main/java/com/team841/dory/RobotContainer.java
@@ -31,6 +31,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -189,8 +190,8 @@ public class RobotContainer {
         //         .whileTrue(new InstantCommand(()->this.flapSystem.setFlapperDutyCycle(-0.25),flapSystem))
         //         .onFalse(new InstantCommand(()->this.flapSystem.stopFlapper(),flapSystem));
 
-        cojoystick.povRight().onTrue(new InstantCommand(() -> this.flapSystem.setFlapperDutyCycle(-0.40), flapSystem).withTimeout(1).finallyDo(flapSystem::stopFlapper));
-        
+        cojoystick.povRight().onTrue(new RunCommand(() -> this.flapSystem.setFlapperDutyCycle(-0.40), flapSystem).withTimeout(1).finallyDo(flapSystem::stopFlapper));
+
         joystick.L1()
                 .whileTrue(new InstantCommand(()->this.flapSystem.setHangDutyCycle(-.7),flapSystem))
                 .onFalse(new InstantCommand(()->this.flapSystem.stopHang(),flapSystem));
