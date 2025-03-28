c9a872b69c7e0266297f35266892f16d7e865bf0
David Tian <dtian.github@outlook.com>
2025-03-27 21:25:00 -0700

    upload

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 72;
-  public static final String GIT_SHA = "14855e0b0816725fff08180b7e50deb673033b03";
-  public static final String GIT_DATE = "2025-03-27 20:51:59 PDT";
+  public static final int GIT_REVISION = 73;
+  public static final String GIT_SHA = "2284c31f2a1e312d925507eadb8907e12aac9b3f";
+  public static final String GIT_DATE = "2025-03-27 21:21:30 PDT";
   public static final String GIT_BRANCH = "autos_ccr";
-  public static final String BUILD_DATE = "2025-03-27 21:19:10 PDT";
-  public static final long BUILD_UNIX_TIME = 1743135550361L;
+  public static final String BUILD_DATE = "2025-03-27 21:24:49 PDT";
+  public static final long BUILD_UNIX_TIME = 1743135889833L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/com/team841/dory/drive/Drivetrain.java
+++ src/main/java/com/team841/dory/drive/Drivetrain.java
@@ -53,13 +53,13 @@ public class Drivetrain extends SubsystemBase {
 
     public PIDController controller = new PIDController(4, 0, 0.2);
     public ProfiledPIDController vxController = new ProfiledPIDController(
-        0.72918, 0, 0.61981,
+        24.531, 0, 0.8503,
             new TrapezoidProfile.Constraints(
                     4.7, 3.75) // max velocity, max acceleration
     );
 
     public ProfiledPIDController vyController = new ProfiledPIDController(
-        0.72918, 0, 0.61981,
+        24.531, 0, 0.8503,
             new TrapezoidProfile.Constraints(
                     4.7, 3.75) // max velocity, max acceleration
     );
