a9586cb26cf8668ae3627cd31384ffc4ba8eabb4
David Tian <dtian.github@outlook.com>
2025-03-28 11:10:04 -0700

    Update at 'Fri Mar 28 11:10:04 PDT 2025'

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 83;
-  public static final String GIT_SHA = "5b9c1ecb00b2bd4e62834e8e50054a3a0e73a197";
-  public static final String GIT_DATE = "2025-03-28 09:43:20 PDT";
+  public static final int GIT_REVISION = 84;
+  public static final String GIT_SHA = "96d7cc14bca8431e309672e46e4f6ed256dae908";
+  public static final String GIT_DATE = "2025-03-28 11:09:43 PDT";
   public static final String GIT_BRANCH = "event_ccr";
-  public static final String BUILD_DATE = "2025-03-28 09:43:20 PDT";
-  public static final long BUILD_UNIX_TIME = 1743180200241L;
+  public static final String BUILD_DATE = "2025-03-28 11:09:43 PDT";
+  public static final long BUILD_UNIX_TIME = 1743185383419L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
--- src/main/java/com/team841/dory/RobotContainer.java
+++ src/main/java/com/team841/dory/RobotContainer.java
@@ -189,7 +189,7 @@ public class RobotContainer {
         // cojoystick.povRight()
         //         .whileTrue(new InstantCommand(()->this.flapSystem.setFlapperDutyCycle(-0.25),flapSystem))
         //         .onFalse(new InstantCommand(()->this.flapSystem.stopFlapper(),flapSystem));
-`
+
         cojoystick.povRight().onTrue(new RunCommand(() -> this.flapSystem.setFlapperDutyCycle(-0.5), flapSystem).withTimeout(0.5).finallyDo(flapSystem::stopFlapper));
 
         joystick.L1()
