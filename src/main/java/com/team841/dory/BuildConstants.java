94b1c6ce0e1b1ae02341489bdefcba4595d63cd7
David Tian <dtian.github@outlook.com>
2025-03-28 12:58:06 -0700

    Update at 'Fri Mar 28 12:58:06 PDT 2025'

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 89;
-  public static final String GIT_SHA = "a24edbb735a195e23cfbaae8e84ea861fcdb0853";
-  public static final String GIT_DATE = "2025-03-28 12:14:34 PDT";
+  public static final int GIT_REVISION = 90;
+  public static final String GIT_SHA = "b317d444b97b91c7933a69174905528e001c51cc";
+  public static final String GIT_DATE = "2025-03-28 12:34:43 PDT";
   public static final String GIT_BRANCH = "event_ccr";
-  public static final String BUILD_DATE = "2025-03-28 12:14:34 PDT";
-  public static final long BUILD_UNIX_TIME = 1743189274788L;
+  public static final String BUILD_DATE = "2025-03-28 12:34:43 PDT";
+  public static final long BUILD_UNIX_TIME = 1743190483606L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
