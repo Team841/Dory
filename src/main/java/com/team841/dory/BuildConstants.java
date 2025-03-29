d58f78eb2e7125a2c5fed8452e6fb7968ba25279
David Tian <dtian.github@outlook.com>
2025-03-29 08:53:52 -0700

    Update at 'Sat Mar 29 08:53:52 PDT 2025'

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 90;
-  public static final String GIT_SHA = "81c4bbeb8a2b3ead910fbeb701873ede9970ccbb";
-  public static final String GIT_DATE = "2025-03-29 08:50:59 PDT";
+  public static final int GIT_REVISION = 91;
+  public static final String GIT_SHA = "358058a30b5e36485b54e75af7df3515785203be";
+  public static final String GIT_DATE = "2025-03-29 08:52:33 PDT";
   public static final String GIT_BRANCH = "event_ccr3";
-  public static final String BUILD_DATE = "2025-03-29 08:50:59 PDT";
-  public static final long BUILD_UNIX_TIME = 1743263459483L;
+  public static final String BUILD_DATE = "2025-03-29 08:52:33 PDT";
+  public static final long BUILD_UNIX_TIME = 1743263553960L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
--- src/main/java/com/team841/dory/constants/SC.java
+++ src/main/java/com/team841/dory/constants/SC.java
@@ -44,9 +44,9 @@ public class SC {
                                         .withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor))
                         .withMotionMagic(
                                 new MotionMagicConfigs()
-                                        .withMotionMagicAcceleration(90)
-                                        .withMotionMagicCruiseVelocity(120)
-                                        .withMotionMagicJerk(1200))
+                                        .withMotionMagicAcceleration(120)
+                                        .withMotionMagicCruiseVelocity(150)
+                                        .withMotionMagicJerk(1500))
                         .withMotorOutput(
                                 new MotorOutputConfigs()
                                         .withNeutralMode(NeutralModeValue.Brake))
