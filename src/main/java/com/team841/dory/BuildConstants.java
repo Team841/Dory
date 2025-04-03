257f0d539a5b9f84298903c77c5a252445248fb4
David Tian <dtian.github@outlook.com>
2025-04-02 20:04:28 -0700

    Update at 'Wed Apr 02 20:04:28 PDT 2025'

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 124;
-  public static final String GIT_SHA = "dd03b2d2a9ad8dd8ebd0e76ea6ff42a8f5b61d86";
-  public static final String GIT_DATE = "2025-04-02 20:01:37 PDT";
+  public static final int GIT_REVISION = 125;
+  public static final String GIT_SHA = "c86eb277d645a8957cd6343c06fe1c06856b1ddf";
+  public static final String GIT_DATE = "2025-04-02 20:02:29 PDT";
   public static final String GIT_BRANCH = "event_preEBR";
-  public static final String BUILD_DATE = "2025-04-02 20:01:37 PDT";
-  public static final long BUILD_UNIX_TIME = 1743649297512L;
+  public static final String BUILD_DATE = "2025-04-02 20:02:29 PDT";
+  public static final long BUILD_UNIX_TIME = 1743649349847L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
