6898318a22e49008df9ee9979631e09ae55704a1
David Tian <dtian.github@outlook.com>
2025-04-03 18:35:04 -0700

    Update at 'Thu Apr 03 18:35:04 PDT 2025'

--- src/main/java/com/team841/dory/BuildConstants.java
+++ src/main/java/com/team841/dory/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dory";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 156;
-  public static final String GIT_SHA = "09b7d588517844e782ad9b727c3910f3fc541950";
-  public static final String GIT_DATE = "2025-04-03 18:25:21 PDT";
+  public static final int GIT_REVISION = 157;
+  public static final String GIT_SHA = "9644ad9d355bc27dc79296ed4bec3dda4e907b07";
+  public static final String GIT_DATE = "2025-04-03 18:32:56 PDT";
   public static final String GIT_BRANCH = "event_preEBR";
-  public static final String BUILD_DATE = "2025-04-03 18:25:21 PDT";
-  public static final long BUILD_UNIX_TIME = 1743729921624L;
+  public static final String BUILD_DATE = "2025-04-03 18:32:56 PDT";
+  public static final long BUILD_UNIX_TIME = 1743730376653L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
--- src/main/java/com/team841/dory/drive/Drivetrain.java
+++ src/main/java/com/team841/dory/drive/Drivetrain.java
@@ -239,7 +239,7 @@ public class Drivetrain extends SubsystemBase {
                     (speeds, feedforwards) -> io.setControl(
                             m_pathApplyRobotSpeeds.withSpeeds(speeds).withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons()).withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())), new PPHolonomicDriveController(
                                     // PID constants for translation
-                                    new PIDConstants(6, 0, 0),
+                                    new PIDConstants(6.75, 0, 0),
                                     // PID constants for rotation
                                     new PIDConstants(5, 0, 0)), config,
                     // Assume the path needs to be flipped for Red vs Blue, this is normally the case
